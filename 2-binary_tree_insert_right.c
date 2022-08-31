26e8f03e6c280e4a2ebc71d2243d1070cfd32253
Ignacio Castro <ignaciocastro347@gmail.com>
2022-08-31 19:53:25 -0300

    Create a function that inserts a node as the right-child of another node.

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,1 +1,29 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - insert a node to the right
+ * @parent: parent node
+ * @value: int value
+ * Return: node value
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
+
+	if (parent->right)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+
+	parent->right = new_node;
+
+	return (new_node);
+}
