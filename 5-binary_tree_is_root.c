95b791fd7ff8567dcc8c22040247e86e5420cdba
Ivan Hansen <ihk225@gmail.com>
2022-08-31 20:35:39 -0300

    Exercise 4

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,1 +1,17 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - check if node is a root node
+ * 
+ * @node: node to check
+ * Return: 1 if node is a root node, 0 otherwise
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->parent && !node->left && !node->right)
+		return (1);
+	return (0);
+}
