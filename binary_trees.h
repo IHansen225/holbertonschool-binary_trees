62f6d087e76b76b72eafef7fa202b24935507121
Ignacio Castro <ignaciocastro347@gmail.com>
2022-08-31 20:27:57 -0300

    Try to fix static function error

--- binary_trees.h
+++ binary_trees.h
@@ -60,7 +60,6 @@ binary_tree_t *binary_tree_parent(binary_tree_t *node);
 
 /* ############# UTILS ############# */
 
-size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif
