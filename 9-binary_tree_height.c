eee4a6eab4e1c6aa5532f6f88c352515835a1ccc
Ivan Hansen <ihk225@gmail.com>
2022-08-31 22:22:46 -0300

    Exercise 9 first commit

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,1 +1,18 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_height - calculates height of the tree
+ * 
+ * @tree: binary tree
+ * Return: height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l;
+	size_t r;
+
+	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (l > r ? l : r);
+}
