d7f99d8374a0bf38dc0ddfd995cae4f1ee573e89
Ivan Hansen <ihk225@gmail.com>
2022-08-31 22:23:27 -0300

    Exercise 9 first commit

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,9 +9,10 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l;
-	size_t r;
+	size_t l, r;
 
+	if (!tree)
+		return (0);
 	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (l > r ? l : r);
