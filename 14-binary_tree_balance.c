008b2d307785eddd19e17aebff0b5bf5e2cb8c15
Ivan <ihk225@gmail.com>
2022-09-01 18:12:46 -0300

    Exercise 14 first commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,1 +1,19 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_balance - checks if a binary tree is balanced
+ *
+ * @tree: binary tree
+ * Return: balance factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t l, r;
+
+	if (!tree)
+		return (0);
+	l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
+	r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	return (l - r);
+}
