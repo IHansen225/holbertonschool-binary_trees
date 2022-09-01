e8bbc025595c90ad3f01d6f016ece549617d431c
Ivan <ihk225@gmail.com>
2022-09-01 18:45:42 -0300

    Exercise 15 first commit

--- 14-main.c
+++ 14-main.c
@@ -1,6 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the number of leaves in the tree
+ *
+ * @tree: binary tree
+ * Return: number of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
 
 /**
  * main - Entry point
