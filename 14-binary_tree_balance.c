b1a4e093ffec69acd49169643631fbd1d958f07f
Ivan <ihk225@gmail.com>
2022-09-01 18:21:43 -0300

    Exercise 14 fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,24 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - calculates height of the tree
+ *
+ * @tree: binary tree
+ * Return: height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l, r;
+
+	if (!tree)
+		return (0);
+	l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+	r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+	return (l > r ? l : r);
+}
+
+/**
  * binary_tree_balance - checks if a binary tree is balanced
  *
  * @tree: binary tree
@@ -13,7 +31,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	l = binary_tree_height(tree->left);
+	r = binary_tree_height(tree->right);
+	printf("l: %ld r: %ld\n", l, r);
 	return (l - r);
 }
