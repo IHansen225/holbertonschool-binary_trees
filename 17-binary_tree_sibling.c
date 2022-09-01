4d3cf771461ce661ce701b1fce52f5c38a6ea6ab
Ivan <ihk225@gmail.com>
2022-09-01 20:14:20 -0300

    Exercise 17 & 18 first commit

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,1 +1,17 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - returns the sibling of a binary tree node
+ * 
+ * @node: binary tree node
+ * Return: node's sibling
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,1 +1,31 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - returns the sibling of a binary tree node
+ *
+ * @node: binary tree node
+ * Return: node's sibling
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
+
+/**
+ * binary_tree_uncle
+ *
+ * @node: binary tree node
+ * Return: pointer to a binary node's uncle
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if(!node || !node->parent)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
+}
