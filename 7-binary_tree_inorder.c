4c47bc30cbb74e4b0e58d35c871bd8956708a9a1
Ivan Hansen <ihk225@gmail.com>
2022-08-31 22:15:41 -0300

    Exercise 7 first commit

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,1 +1,23 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - preorder traversal of a binary tree
+ *
+ * @tree: binary tree
+ * @func: function to call
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	if (tree->left)
+	{
+		binary_tree_preorder(tree->left, func);
+	}
+	func(tree->n);
+	if (tree->right)
+	{
+		binary_tree_preorder(tree->right, func);
+	}
+}
