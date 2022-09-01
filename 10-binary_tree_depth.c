0eddb3d13767ab65b92899152f803e7d32d03d6a
Ivan Hansen <ihk225@gmail.com>
2022-08-31 22:28:53 -0300

    Exercise 10 first commit

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,1 +1,22 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_depth - calculates the depth of a binary tree node
+ *
+ * @tree: pointer to the node 
+ * Return: depth
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if(!tree)
+		return (depth);
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
+}
