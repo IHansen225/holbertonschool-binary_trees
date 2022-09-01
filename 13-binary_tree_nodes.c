76604ae2681e1dd6427da7e27accf92bbdf09a6a
Ivan <ihk225@gmail.com>
2022-09-01 17:53:21 -0300

    Exercises 11, 12, 13 first commit

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,1 +1,17 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - returns the number of nodes in the tree
+ *
+ * @tree: binary tree
+ * Return: number of nodes
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+		return (binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right));
+	return (0);
+}
